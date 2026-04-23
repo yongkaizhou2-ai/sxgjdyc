8c6ae447c8989bdcbd6e51d477a94076a578ab1a
yongkaizhou2-ai <yongkaizhou2@gmail.com>
2026-04-23 15:55:31 +0800

    Create FloorMap.vue component for heatmap visualization

---
+++ src/components/FloorMap.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="floor-map">
+    <canvas ref="heatmapCanvas" width="800" height="600"></canvas>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FloorMap',
+  mounted() {
+    this.initializeHeatmap();
+  },
+  methods: {
+    initializeHeatmap() {
+      const canvas = this.$refs.heatmapCanvas;
+      const ctx = canvas.getContext('2d');
+      // Heatmap initialization logic goes here
+    }
+  }
+};
+</script>
+
+<style scoped>
+.floor-map {
+  position: relative;
+  width: 800px;
+  height: 600px;
+}
+</style>
